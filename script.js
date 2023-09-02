79f3f5535096b007aa8bba91796bd5bace6e5448
daniel <137113985+DanMa4566@users.noreply.github.com>
2023-09-02 16:36:22 -0400

    add change grid size button functionality
    asks the user how big the 'canvas' will be, value cannot be higher than 100, negative or not a number, rows and columns are created differently now

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
 const etchASketch = document.querySelector(".container")
+const button = document.querySelector("#change")
+let rowsAndColumns = 0
+let oldRowsAndColumns = 0
 
+function changeGridSize() {
+    while (true) {
+        oldRowsAndColumns = rowsAndColumns
+        rowsAndColumns = Number(prompt("How many squares do you want in each row and column, the value cannot be higher than 100"))
 
-
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div')
-    div.classList.add("pixel")
-    div.addEventListener("mouseover", () => {
-        div.classList.add("pixel-hovered")
-    })
-    div.textContent = "A"
-    etchASketch.appendChild(div)
+        if (rowsAndColumns <= 100 && rowsAndColumns > 0) {
+            addRowsAndColumns(oldRowsAndColumns, rowsAndColumns);
+            break;
+        } else {
+            alert("The value cannot be higher than 100, be negative or a random word")
+        }
+    }
 }
+
+button.addEventListener("click", changeGridSize)
+
+function addRowsAndColumns(oldRowAndColumn, rowAndColumn) {
+
+    if (oldRowAndColumn) {
+        etchASketch.innerHTML = ""
+    }
+
+    for (let i = 0; i < rowAndColumn; i++) {
+        const column = document.createElement('div')
+        column.classList.add("column")
+
+        for (let j = 0; j < rowAndColumn; j++) {
+            const row = document.createElement('div')
+            row.classList.add("row")
+
+            row.addEventListener("mouseover", () => {
+                row.classList.add("row-hovered")
+            })
+
+            row.textContent = "A"
+            column.appendChild(row)
+        }
+        
+        etchASketch.appendChild(column)
+    }
+}
+
+changeGridSize()
